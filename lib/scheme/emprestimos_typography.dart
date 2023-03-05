6451d838527e73d5eeb692e8cd978fcd3acb0abf
Paulo Ortolan <paulo.ortolan@gmail.com>
2023-03-05 00:21:23 +0000

    Loading and Welcome Screen, registry information

---
+++ lib/scheme/emprestimos_typography.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+enum EmprestimosTypography {
+  mainTitle,
+  itemLabel,
+  itemNotes,
+  editLabel,
+  editField,
+  hideLabel,
+}
+
+extension EmprestimosTypographyExtension on EmprestimosTypography {
+  TextStyle? get textStyle {
+    switch (this) {
+      case EmprestimosTypography.mainTitle:
+        return const TextStyle(
+          fontSize: 26,
+          fontWeight: FontWeight.bold,
+        );
+      case EmprestimosTypography.itemLabel:
+        return const TextStyle(
+          fontSize: 24,
+        );
+      case EmprestimosTypography.itemNotes:
+        return const TextStyle(
+          fontSize: 16,
+        );
+      case EmprestimosTypography.editLabel:
+        return const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        );
+      case EmprestimosTypography.editField:
+        return const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+          color: Color.fromARGB(0xff, 0x00, 0x66, 0xCC),
+        );
+      case EmprestimosTypography.hideLabel:
+        return const TextStyle(
+          fontSize: 18,
+        );
+    }
+  }
+}
