4462ded82c1c6fe83bddc2fdd0080c67bb284c96
Paulo Ortolan <paulo.ortolan@gmail.com>
2023-03-20 02:26:09 +0000

    Enum de Operacao Movimentacao

--- lib/entities/operacao_movimentacao.dart
+++ lib/entities/operacao_movimentacao.dart
@@ -1,3 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
 enum OperacaoMovimentacao {
   emprestimo, pagamento
+}
+
+extension OperacaoMovimentacaoExtension on OperacaoMovimentacao {
+
+  Icon? get icon {
+    return null;
+  }
+
 }
